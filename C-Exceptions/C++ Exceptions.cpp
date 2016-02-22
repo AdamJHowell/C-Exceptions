b794ea5a826344363612f4d2c384a76b80e9278d
Adam Howell <adam.j.howell@gmail.com>
2016-02-22 13:51:47 -0700

    Initial commit
    Mostly just setup the .gitignore and added some sample code from
    http://www.cplusplus.com/doc/tutorial/exceptions/

---
+++ C-Exceptions/C++ Exceptions.cpp
@@ -0,0 +1,16 @@
+// C++ exceptions
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	try
+	{
+		throw 20;
+	}
+	catch( int e )
+	{
+		cout << "An exception occurred. Exception Nr. " << e << '\n';
+	}
+	return 0;
+}
